d4e1d58825511280cd794e54e091eec2de9ce6df
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-08 19:34:19 +0100

    #005: added constants for creating JWTs + created a service to work with JWT

---
+++ src/main/java/com/makridin/instazoo/security/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.makridin.instazoo.security;
+
+public interface SecurityConstants {
+
+    String SIGN_UP_URLS = "/api/auth/**";
+    String SECRET = "SecretKeyGenJWT";
+    String TOKEN_PREFIX = "Bearer ";
+    String HEADER_STRING = "Authorization";
+    String CONTENT_TYPE = "application/json";
+    long EXPIRATION_TIME = 600000;
+}
