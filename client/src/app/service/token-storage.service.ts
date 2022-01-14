aec0f9a803a9d52ad444eaa67153ef27d986f7e7
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-14 23:49:57 +0100

    #035: client: turned off null check for methods in config + implemented methods for token-storage.service

--- client/src/app/service/token-storage.service.ts
+++ client/src/app/service/token-storage.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,27 @@ import { Injectable } from '@angular/core';
 export class TokenStorageService {
 
   constructor() { }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  private saveUser(user: User): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): User {
+    return JSON.parse(window.sessionStorage.getItem(USER_KEY));
+  }
+
+  public logOut(): void {
+    window.sessionStorage.clear();
+    window.location.reload;
+  }
 }
