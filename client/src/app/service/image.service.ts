1d7f800ba9f712bd80988771921ea1fc46bfb5aa
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-15 19:09:46 +0100

    #042: implemented 4 methods for image service(upload profile image, upload post image, get profile image and get post image)

--- client/src/app/service/image.service.ts
+++ client/src/app/service/image.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_URL } from './service-consts';
+
+export const IMG_API = API_URL + 'image/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  uploadProfileImage(file: File): Observable<any> {
+    const uploadData = new FormData();
+    uploadData.append("file", file);
+
+    return this.http.post(IMG_API + 'upload', uploadData);
+  }
+
+  uploadPostImage(file: File, postId: number): Observable<any> {
+    const uploadData = new FormData();
+    uploadData.append("file", file);
+
+    return this.http.post(IMG_API + postId + '/upload', uploadData);
+  }
+
+  getProfileImage(): Observable<any> {
+    return this.http.get(IMG_API + "/profile");
+  }
+
+  getPostImage(postId: number): Observable<any> {
+    return this.http.get(IMG_API + postId + "/image");
+  }
 }
