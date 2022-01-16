82c44c80e71c1ade287dbe92e37852d1583ecb1d
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-16 17:48:18 +0100

    #048: client: added logic + layout + some styles in login component

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { NotificationService } from 'src/app/service/notification.service';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenStorageService,
+    private notificationService: NotificationService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    if (this.tokenService.getUser) {
+      this.router.navigate(['main']);
+    }
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.createLoginForm();
+  }
+
+  createLoginForm(): FormGroup {
+    return this.fb.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    })
+  }
+
+  submit() {
+    this.authService.login({
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    }).subscribe(data => {
+      console.log(data);
+      this.tokenService.saveToken(data.token);
+      this.tokenService.saveUser(data);
+      this.notificationService.showSncackBar('Successfully logged in');
+      this.router.navigate(['/']);
+      window.location.reload();
+    }, error => {
+      console.error(error);
+      this.notificationService.showSncackBar(error);
+    });
   }
 
 }
