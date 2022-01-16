65f887a17e5ab9510b481c8071ba009e8a79de7b
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-16 11:41:48 +0100

    #044: client: implemented 2 methods for auth service(login and registration) + added several fields into User

--- client/src/app/models/User.ts
+++ client/src/app/models/User.ts
@@ -5,4 +5,6 @@ export interface User {
     firstname: string;
     lastname: string;
     bio: string;
+    password?: string;
+    confirmedPassword?: string;
 }
--- client/src/app/service/auth.service.ts
+++ client/src/app/service/auth.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/User';
+import { API_URL } from './service-consts';
+
+export const AUTH_API = API_URL + 'auth/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user: User): Observable<any> {
+    return this.http.post(AUTH_API + 'signin', {
+      username: user.username,
+      password: user.password
+    });
+  }
+
+  register(user: User): Observable<any> {
+    return this.http.post(AUTH_API + 'signup', {
+      username: user.username,
+      email: user.email,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      password: user.password,
+      confirmedPassword: user.confirmedPassword
+    });
+  }
 }
