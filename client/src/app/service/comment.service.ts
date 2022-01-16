af3632815682ff414b893809ec98196eea361bf7
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-16 10:11:26 +0100

    #043: client: implemented 3 methods for comment service(create, getAllPostComments, delete)

--- client/src/app/service/comment.service.ts
+++ client/src/app/service/comment.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_URL } from './service-consts';
+
+export const COMMENT_API = API_URL + 'comment/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addPostComment(postId: number, message: string): Observable<any> {
+    return this.http.post(COMMENT_API + postId + '/create', {
+      message: message
+    });
+  }
+
+  getPostComments(postId: number): Observable<any> {
+    return this.http.get(COMMENT_API + postId + '/comments');
+  }
+
+  deletePostComment(commentId: number,): Observable<any> {
+    return this.http.delete(COMMENT_API + commentId);
+  }
 }
